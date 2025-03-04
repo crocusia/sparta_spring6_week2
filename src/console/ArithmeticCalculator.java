7f4cb7a595e44486cabc99bb85bdbd87ffef56fd
crocusia <132359536+crocusia@users.noreply.github.com>
2025-03-04 18:10:58 +0900

    modify enum OperationType for OOD

--- src/console/ArithmeticCalculator.java
+++ src/console/ArithmeticCalculator.java
@@ -4,58 +4,58 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-//lv3(도전 기능) 요구사항 - Enum 타입을 활용하여 연산자 타입에 대한 정보를 관리
-enum OperationType{
-    ADD('+'){
-        @Override
-        public <T1 extends Number, T2 extends Number> Number apply(T1 num1, T2 num2){
-            return num1.doubleValue() + num2.doubleValue();
-        }
-    },
-    SUB('-'){
-        @Override
-        public <T1 extends Number, T2 extends Number> Number apply(T1 num1, T2 num2){
-            return num1.doubleValue() - num2.doubleValue();
-        }
-    },
-    MUL('*'){
-        @Override
-        public <T1 extends Number, T2 extends Number> Number apply(T1 num1, T2 num2){
-            return num1.doubleValue() * num2.doubleValue();
-        }
-    },
-    DIV('/'){
-        @Override
-        public <T1 extends Number, T2 extends Number> Number apply(T1 num1, T2 num2){
-            if(num2.doubleValue() == 0.0){
-                throw new ArithmeticException();
-            }
-            return num1.doubleValue() / num2.doubleValue();
-        }
-    };
-    //생성자
-    private final char operator;
-    OperationType(char operator){
-        this.operator = operator;
-    }
-
-    //추상 메서드 lv3(도전 기능) 요구사항 - 제네릭을 사용한 타입 무관 연산 수행
-    public abstract <T1 extends Number, T2 extends Number> Number apply(T1 num1, T2 num2);
-
-    //연산 기호 판별
-    public static OperationType checkChar(char operator) {
-        for(OperationType op : OperationType.values()){
-            if(op.operator == operator){
-                return op;
-            }
-        }
-        throw new IllegalArgumentException("연산자: " + operator);
-    }
-}
-
 public class ArithmeticCalculator {
 
     private final List<Number> results = new ArrayList<>();
+
+    //lv3(도전 기능) 요구사항 - Enum 타입을 활용하여 연산자 타입에 대한 정보를 관리
+    private static enum OperationType{
+        ADD('+'){
+            @Override
+            public <T1 extends Number, T2 extends Number> Number apply(T1 num1, T2 num2){
+                return num1.doubleValue() + num2.doubleValue();
+            }
+        },
+        SUB('-'){
+            @Override
+            public <T1 extends Number, T2 extends Number> Number apply(T1 num1, T2 num2){
+                return num1.doubleValue() - num2.doubleValue();
+            }
+        },
+        MUL('*'){
+            @Override
+            public <T1 extends Number, T2 extends Number> Number apply(T1 num1, T2 num2){
+                return num1.doubleValue() * num2.doubleValue();
+            }
+        },
+        DIV('/'){
+            @Override
+            public <T1 extends Number, T2 extends Number> Number apply(T1 num1, T2 num2){
+                if(num2.doubleValue() == 0.0){
+                    throw new ArithmeticException();
+                }
+                return num1.doubleValue() / num2.doubleValue();
+            }
+        };
+        //생성자
+        private final char operator;
+        OperationType(char operator){
+            this.operator = operator;
+        }
+
+        //추상 메서드 lv3(도전 기능) 요구사항 - 제네릭을 사용한 타입 무관 연산 수행
+        public abstract <T1 extends Number, T2 extends Number> Number apply(T1 num1, T2 num2);
+
+        //연산 기호 판별
+        public static OperationType checkChar(char operator) {
+            for(OperationType op : OperationType.values()){
+                if(op.operator == operator){
+                    return op;
+                }
+            }
+            throw new IllegalArgumentException("연산자: " + operator);
+        }
+    }
 
     public <T1 extends Number, T2 extends Number> Number calculate(T1 num1, T2 num2, char operator){
         OperationType operation = OperationType.checkChar(operator);
