536cad7c66a54bd1edfddf2a2ec54cf4cfc05ad6
crocusia <132359536+crocusia@users.noreply.github.com>
2025-03-04 17:52:08 +0900

    set basic button event listener

--- src/gui/App.java
+++ src/gui/App.java
@@ -2,8 +2,59 @@ package gui;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class App extends JFrame {
+
+    private JTextField inputField; // 입력 필드
+
+    private String checkInput(String text, String command) {
+        if (command.equals("+") || command.equals("-") ||command.equals("*") || command.equals("/")) {
+            if(text.isEmpty()){ //연산기호가 가장 먼저 입력되면 0을 먼저 넣어주기
+                return "0" + command;
+            }
+            else{ //연산기호가 연속으로 입력되지 않도록 하기
+                char lastChar = text.charAt(text.length() - 1);
+                if(lastChar == '+' || lastChar == '-' || lastChar == '×' || lastChar == '÷'){
+                    return text;
+                }
+                else{
+                    return text + command;
+                }
+            }
+        }
+        else{
+            return text + command;
+        }
+    }
+    public class BtnClickListner implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String command = e.getActionCommand();
+            String currentText = inputField.getText();
+            switch (command){
+                case "record":
+                    break;
+                case "exit":
+                    System.exit(0);
+                    break;
+                case "⌫":
+                    if(!currentText.isEmpty()){
+                        inputField.setText(currentText.substring(0, currentText.length()-1));
+                    }
+                    break;
+                case "+/-":
+                    break;
+                case "=":
+                    break;
+                default:
+                    //숫자 또는 연산기호
+                    inputField.setText(currentText + command);
+                    break;
+            }
+        }
+    }
     public App(){
         setTitle("Sparta Calculator");
         setSize(300, 400);
@@ -16,16 +67,19 @@ public class App extends JFrame {
         JPanel toolPanel = new JPanel();
         toolPanel.setLayout(new BorderLayout());
         JButton recordBtn = new JButton("record");
+        recordBtn.addActionListener(new BtnClickListner());
         JButton exitBtn = new JButton("exit");
+        exitBtn.addActionListener(new BtnClickListner());
         toolPanel.add(recordBtn, BorderLayout.WEST);
         toolPanel.add(exitBtn, BorderLayout.EAST);
         //입력창
         JPanel inputPanel = new JPanel();
         inputPanel.setLayout(new BorderLayout());
-        JTextField inputField = new JTextField();
+        inputField = new JTextField();
         inputField.setHorizontalAlignment(JTextField.RIGHT); //우측 정렬
         inputField.setEditable(false); //우선 키보드 입력을 막음
         JButton backspaceBtn = new JButton("⌫");
+        backspaceBtn.addActionListener(new BtnClickListner());
         inputPanel.add(inputField, BorderLayout.CENTER);
         inputPanel.add(backspaceBtn, BorderLayout.EAST);
         //상단(툴바 + 입력창)
@@ -50,6 +104,7 @@ public class App extends JFrame {
 
         for(String l : labels){
             JButton btn = new JButton(l);
+            btn.addActionListener(new BtnClickListner());
             btnPanel.add(btn);
         }
 
